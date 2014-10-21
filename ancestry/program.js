22a6c46998492a5174729dc5c0f57796c1ebfae1
Kia Fathi <kiafathi@me.com>
2014-10-21 11:28:59 -0700

    (fix) correct ancestry problem

--- ancestry/program.js
+++ ancestry/program.js
@@ -1,32 +1,19 @@
 'use strict';
 var ancestry = JSON.parse(require('./ancestry.js'));
 
-var females = ancestry.filter(function(item){
-  return item.sex === 'f';
+var hash = {};
+ancestry.forEach(function(item){
+  hash[item.name] = item;
 });
 
-var hash = females.reduce(function(prev, cur){
-  prev[cur.name] = {};
-  prev[cur.name].born = cur.born;
-  prev[cur.name].died = cur.died;
-  prev[cur.name].mother = cur.mother;
-  return prev;
-}, {});
+var knowParent = ancestry.filter(function(item){
+    return hash[item.mother] !== undefined;
+  });
 
-var daughters = females.filter(function(item){
-  return hash[item.mother] !== undefined;
-});
+var ans = knowParent.map(function(item){
+    return item.born - hash[item.mother].born;
+  }).reduce(function(total, current){
+    return total + current;
+  }, 0)/knowParent.length;
 
-var sumObj = daughters.reduce(function(sum, cur){
-  if(hash[cur.mother]){
-    sum.sum += cur.born - hash[cur.mother].born;
-    sum.num++;
-    return sum;
-  } else {
-    return sum;
-  }
-}, {'sum': 0, 'num' : 0});
-
-var ans = sumObj.sum/daughters.length;
 console.log(ans);
-
