57fa48c43d1dca97be658f0c83efe893265d2348
Kia Fathi <kiafathi@me.com>
2014-12-10 14:00:00 +0100

    (feat) Complete problem 8 in js but integer overflow

--- euler/problem7.py
+++ euler/problem7.py
@@ -3,3 +3,25 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 
 What is the 10 001st prime number?
 '''
+
+# def nthPrime(n):
+#   count = 1
+#   primes = [2, 3]
+#   while(count < n):
+#     current = primes[len(primes)-1] + 2
+#     index = 0
+#     divisor = primes[index]
+#     print current
+#     count += 1
+#   print primes
+
+# nthPrime(2) nthPrime(3)
+
+li = [1,2,3]
+
+#TODO
+
+try: 
+  print li[3]
+except IndexError:
+  print 'Error!'
