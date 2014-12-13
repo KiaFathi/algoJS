1526b97f11da41d19c81dd13639e4327c74a264f
Kia Fathi <kiafathi@me.com>
2014-12-13 08:49:27 -0800

    (feat) Add linkedList and some methods

---
+++ codeRust/linkedList.js
@@ -0,0 +1,122 @@
+function Node(value){
+  this.value = value;
+  this.next = null;
+}
+
+function LinkedList(){
+  this.head = null;
+  this.tail = null;
+}
+
+LinkedList.prototype.addToTail = function(value){
+  var node = new Node(value);
+  if(this.tail === null){
+    this.tail = node;
+    this.head = node;
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
+};
+
+LinkedList.prototype.addNodeToTail = function(node){
+  if(this.tail === null){
+    this.tail = node;
+    this.head = node;
+  } else {
+    this.tail.next = node;
+    this.tail = node;
+  }
+};
+
+LinkedList.prototype.removeFromHead = function(){
+  var node = this.head;
+  if(this.tail === node){
+    this.tail = null;
+  }
+  if(this.head !== null){
+    this.head = this.head.next;
+  }
+  node.next = null;
+  return node;
+};
+
+LinkedList.prototype.addToHead = function(value){
+  var node = new Node(value);
+  node.next = this.head;
+  if(this.tail === null){
+    this.tail = node;
+  }
+  this.head = node;
+};
+
+LinkedList.prototype.addNodeToHead = function(node){
+  node.next = this.head;
+  if(this.tail === null){
+    this.tail = node;
+  }
+  this.head = node;
+};
+
+LinkedList.prototype.reverse = function(){
+  var stack = [];
+  while(this.head !== null){
+    stack.push(this.removeFromHead());
+  }
+  while(stack.length){
+    this.addNodeToTail(stack.pop());
+  }
+};
+
+LinkedList.prototype.delete = function(value){
+  var node = this.head;
+  if(node.value === value){
+    this.removeFromHead();
+  } else {
+    while(node !== null){
+      var next = node.next;
+      if(next !== null && next.value === value){
+        node.next = next.next;
+      }
+      node = node.next;    
+    }
+  }
+};
+
+LinkedList.prototype.addAnotherLL(linkedList){
+  var node1 = this.head;
+  var node2 = linkedList.head;
+  var carry = 0;
+  var value = 0;
+  while(node1 !== null || node2 !== null){
+    value = node1.value + node2.value;
+
+    node1 = node1.next;
+    node2 = node2.next;
+  }
+
+};
+
+var LL = new LinkedList();
+LL.addToTail(1);
+LL.addToTail(2);
+LL.addToTail(3);
+console.log(LL);
+LL.reverse();
+console.log(LL);
+LL.addToTail(4);
+LL.addToTail(5);
+LL.delete(3);
+console.log(LL);
+LL.delete(1);
+console.log(LL);
+
+var LL1 = new LinkedList();
+LL1.addToTail(4);
+LL1.addToTail(5);
+LL1.addToTail(6);
+
+var LL2 = new LinkedList();
+LL2.addToTail(7);
+LL2.addToTail(8);
+LL2.addToTail(9);
