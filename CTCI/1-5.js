88d792eaa19c2ca241f64a7ff3fa154b0ca9db79
Kia Fathi <kiafathi@me.com>
2015-01-01 20:36:43 -0800

    (feat) CTCI 1-5

---
+++ CTCI/1-5.js
@@ -0,0 +1,30 @@
+/*
+Write a function that does the following:
+
+Input: "AAABBBCCCDDDAAA"
+Output: "A3B3C3D3A3"
+
+*/
+
+function compressor(str){
+  var resArray = [];
+  var prev = str[0];
+  var count = 1;
+
+  for(var i = 1; i < str.length; i++){
+    var cur = str[i];
+    //check if current character is equal to previous
+    if(prev === cur){
+      count++;
+    } else if(prev){
+      resArray.push(prev + count);
+      prev = cur;
+      count = 1;
+    }
+  }
+  resArray.push(prev + count);
+  return resArray.join('');
+}
+
+console.log(compressor('AAABBBCCCDDDAAA'));
+console.log(compressor('CCCDDEFGHIJKABCCCCCCCCCCCC'));
